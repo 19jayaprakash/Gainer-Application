bbf13d0d87ea34c2f337eca125f7096559e5abfb
ktmjk <101854990+ktmjk@users.noreply.github.com>
2023-08-04 09:35:07 +0530

    Add project files.

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link, Navigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Service Provider</h1>
+          <p className="lead">
+            Create a service provider profile.
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Provider's register
+            </Link>
+            <Link to="/register2" className="btn btn-light">
+              User's register
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
---
+++ config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = "mongodb://127.0.0.1:27017/gainer-application";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
